0616b99c93aed32f5ccad1e19aa2b3b514a2dfeb
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-04-24 17:48:54 -0300

    feat: create subscriber

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,35 @@
+import { gql, useMutation } from "@apollo/client";
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 
+const CREATE_SUBSCRIBER_MUTATION = gql`
+  mutation CreateSubscriber($name: String!, $email: String!) {
+    createSubscriber(data: { name: $name, email: $email }) {
+      id
+    }
+  }
+`;
+
 export function Subscribe() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [createSubscriber, { loading }] = useMutation(
+    CREATE_SUBSCRIBER_MUTATION,
+  );
+
+  async function handleSubscribe(event: FormEvent) {
+    event.preventDefault();
+    await createSubscriber({
+      variables: { name, email },
+    });
+
+    navigate("/event");
+  }
+
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
@@ -24,20 +53,26 @@ export function Subscribe() {
             Inscreva-se gratuitamente
           </strong>
 
-          <form className="flex flex-col gap-2 w-full">
+          <form
+            onSubmit={handleSubscribe}
+            className="flex flex-col gap-2 w-full"
+          >
             <input
               type="text"
               placeholder="Seu nome completo"
+              onChange={event => setName(event.target.value)}
               className="bg-gray-900 rounded px-5 h-14"
             />
             <input
               type="email"
               placeholder="Seu melhor e-mail"
+              onChange={event => setEmail(event.target.value)}
               className="bg-gray-900 rounded px-5 h-14"
             />
             <button
               type="submit"
-              className="mt-4 bg-green-500 py-4 uppercase rounded font-bold text-sm hover:bg-green-700 transition-colors"
+              disabled={loading}
+              className="mt-4 bg-green-500 py-4 uppercase rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
             >
               Garantir minha vaga
             </button>
